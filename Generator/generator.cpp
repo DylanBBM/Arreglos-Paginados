33b1245fdf4e81f38d2ed1cd76ec952b88f642b2
DylanBBM <d.bonilla.3@estudiantec.cr>
2026-03-07 23:13:59 -0600

    Generador ahora acepta formato: >>generator –size <SIZE> -output <OUTPUT FILE PATH>

--- Generator/generator.cpp
+++ Generator/generator.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
-#include <fstream> //Para trabajor con archivos
+#include <fstream> // Para trabajar con archivos
 #include <random>
-#include <cstdint> //Para asegurarse siempre que los numeros sean de 4bytes
+#include <cstdint> // Para asegurarse siempre que los numeros sean de 4 bytes
 
+int main(int argc, char* argv[]){ 
 
-int main(){
+    if(argc != 5){
+        std::cout << "Uso correcto: $> generator -size <SIZE> -output <OUTPUT FILE PATH>" << '\n';
+        std::cout << "SIZE Puede ser: SMALL (512MB) MEDIUM (1024MB) LARGE (2048MB)" << '\n';
+        std::cout << "<OUTPUT FILE PATH> corresponde a la ruta del sistema de archivos donde se guarda el archivo generador." << '\n';
+        std::cout << "Presione ENTER para salir" << std::endl;
+        std::cin.get();
+        return 1;
+    }
 
-    std::ofstream BinaryFile("BinaryIntFile.dat", std::ios::binary);
+    // Obtener el tamaño del archivo 
+    std::string size = argv[2];
 
+    long long Limit = 0; // Numero de enteros de 4 bytes
+    if(size == "SMALL") {
+        //Numero de enteros:
+        Limit = 512LL * 1024 * 1024 / 4; 
+    } else if(size == "MEDIUM") {
+        Limit = 1024LL * 1024 * 1024 / 4; 
+    } else if(size == "LARGE") {
+        Limit = 2048LL * 1024 * 1024 / 4; 
+    } else {
+        std::cout << "Error, la casilla <SIZE> debe ser SMALL, MEDIUM o LARGE" << '\n';
+        std::cout << "Presione ENTER para salir" << std::endl;
+        std::cin.get();
+        return 1;
+    }
+
+    // Obtener la ruta
+    std::string outputFilePath = argv[4];
+    //Objeto para escribir
+    std::ofstream BinaryFile(outputFilePath, std::ios::binary);
+    if(!BinaryFile){
+        std::cout << "Error no se pudo abrir el archivo" << '\n';
+        std::cout << "Presiona Enter para salir" << std::endl;
+        std::cin.get();
+        return 1;
+    }
+    //Hace una semilla para los numeros random
     std::mt19937 randomSeedGenerator(std::random_device{}());
-    //Numeros de 4 bytes SIEMPRE SEA CUAL SEA LA COMPUTADORA
     int32_t randomNumber;
 
-    //Tecnicamente con int sirve, pero long/long long es mas seguro.
-    long long Limit = 134217728;
-
-    for(long long i = 0; i<Limit; i++){
+    //Hasta el limite
+    for(long long i = 0; i < Limit; i++){
         randomNumber = randomSeedGenerator();
-
-        //Write funciona: (dir EN CHAR , tamanio)
+        //Write (direccion char, tamanio)
         BinaryFile.write((char*)&randomNumber, sizeof(int32_t));
-
     }
     BinaryFile.close();
+    //Notificar 
+    std::cout << "Archivo generado correctamente en " << outputFilePath 
+              << "con size de " << Limit << " enteros aleatorios" << '\n';
+    std::cout << "Presione Enter para salir" << std::endl;
+    std::cin.get();
     return 0;
 }
